850e874a0b76f912d30a15f5347c4f665b9229b0
Rafael <RafaelOliveiracds@gmail.com>
2025-11-01 08:32:45 -0300

    C folder

--- C/01-ordenação-alunos/utils/escreverTempo.h
+++
@@ -1,6 +0,0 @@
-void escreverTempo(char nome[], double tempo, char path[], int quantidadeAlunos)
-{
-    FILE *arquivo = fopen(path, "a");
-    fprintf(arquivo, "%s, %d, %f\n", nome, quantidadeAlunos, tempo);
-    fclose(arquivo);
-}
